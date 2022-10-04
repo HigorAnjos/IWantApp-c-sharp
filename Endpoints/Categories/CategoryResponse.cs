9572c81977c14ea87c83f6ee9bb75f546cb8945a
Higor Anjos <higorc.anjos@gmail.com>
2022-10-04 08:22:09 -0300

    feat(get) todas as categorias

---
+++ Endpoints/Categories/CategoryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace IWantApp.Endpoints.Categories
+{
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
+    }
+}
