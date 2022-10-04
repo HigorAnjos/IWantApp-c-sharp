9774066b14317760be10f2d0300a4c19e9266e8b
Higor Anjos <higorc.anjos@gmail.com>
2022-10-04 07:29:24 -0300

    feat(db) Conexao com o banco

---
+++ Domain/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace IWantApp.Domain
+{
+    public class Entity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string EditedBy { get; set; } = string.Empty;
+        public DateTime EditedOn { get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
+    }
+}
---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Properties.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>()
+                .Property(p => p.Description).HasMaxLength(255);
+
+            builder.Entity<Product>()
+                .Property(p => p.Name).HasMaxLength(120).IsRequired();
+
+            //builder.Entity<Product>()
+            //    .Property(p => p.Code).HasMaxLength(20).IsRequired();
+
+            builder.Entity<Category>()
+                .Property(c => c.Name).IsRequired();
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+        {
+            configuration.Properties<string>()
+                .HaveMaxLength(100);
+        }
+    }
+}
